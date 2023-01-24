bf47f596be6ed51a1dcd6d8399a35d8f4a1de2e4
ZOAZ-Uzumaki <timur2006boy@gmail.com>
2023-01-24 22:07:33 +0100

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>;
+#include "..//ConsoleApplication1/steam_api.h";
+
+
+//using namespace std;
+
+
+int main()
+{
+
+	if (SteamAPI_RestartAppIfNecessary(2046801278)) // Replace with your App ID
+	{
+		return 1;
+	}
+
+	if (!SteamAPI_Init())
+	{
+		printf("Fatal Error - Steam must be running to play this game (SteamAPI_Init() failed).\n");
+		return 1;
+
+	}
+
+		
+		 void S_CALLTYPE SteamAPI_RunCallbacks();
+		std::cout << SteamFriends()->GetPersonaName() << std::endl;
+         
+
+	return 0;
+}
